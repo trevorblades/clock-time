65aae8f6dcbeb8668308b45dfec4b1448a8f6ecf
Trevor Blades <tdblades@gmail.com>
2018-03-25 18:47:01 -0700

    Use babel 7, xo, and ava

---
+++ test.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import clockTime from '.';
+
+test('formats milliseconds as clock time', t =>
+  t.is(clockTime(260000), '4:20'));
+
+test('formats minutes and seconds as clock time', t =>
+  t.is(clockTime(4, 20), '4:20'));
+
+test('pad seconds with zeros', t => t.is(clockTime(12, 4), '12:04'));
